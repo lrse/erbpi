5c16ebc2d726d0f256b7ab1ee1a371610541ff7f
javier <javier@4054f5c2-0539-41e8-9b4b-f816069d9907>
2012-02-14 22:39:51 +0000

    ajuste final de normalizaciones para erbpi

--- src/ral/exabot/RAL.cpp
+++ src/ral/exabot/RAL.cpp
@@ -20,7 +20,7 @@ void inicializarRAL(void) {
   //exa_set_sensor(true, SENSOR_012);    usleep(600000); // a veces con 600000 no se llegaban a prender y tiraba todo cero! fijarse si con 800000 no queda demadiado lento...
   //exa_set_sensor(true, SENSOR_345);    usleep(600000);
   //exa_set_sensor(true, SENSOR_67);     usleep(600000);
-  usleep(800000); exa_set_sensor(true, SENSOR_012);    usleep(1000000); // ahora así están prendiendo simepre todos, y no se nota que tarde para arrancar...
+  usleep(600000); exa_set_sensor(true, SENSOR_012);    usleep(1000000); // ahora así están prendiendo simepre todos, y no se nota que tarde para arrancar...
   exa_set_sensor(true, SENSOR_345);    usleep(800000);
   exa_set_sensor(true, SENSOR_67);     usleep(800000);
   exa_set_sensor(true, SENSOR_SONAR);  usleep(300000);
@@ -74,14 +74,13 @@ std::vector<Item> getEstadoSensores(void) {
 	std::vector<std::string> sensorsName = getListaSensores();
 	std::vector<Item> sensors;
 
-	// DUBUGEANDO!
-  cout << "\nRAL:\n\tdistances-telemetros[6](cm) = [ " ;
+  cout << "telemetros[cm] = [ " ;
   for (uint i = 0; i < 6; i++) {
     cout << (unsigned int)distances[i] << " ; ";
   }
   cout << " ]" << endl;
 
-  cout << "\tactivacion-telemetros[6]    = [ " ;
+  cout << "telemetros[%]  = [ " ;
   for (uint i = 0; i < 6; i++) {
     item.id = sensorsName[i];
     float d = distances[i];
@@ -94,7 +93,6 @@ std::vector<Item> getEstadoSensores(void) {
       float normalized = (d - TELEMETER_MIN_DISTANCE) / (TELEMETER_MAX_DISTANCE - TELEMETER_MIN_DISTANCE);
       item.valor = (unsigned int)floorf((1 - normalized) * 100);
     }
-    //cout << "tele " << item.id << " - activacion: " << item.valor << endl;
     cout << item.valor << " ; ";
     sensors.push_back(item);
   }
@@ -108,9 +106,9 @@ std::vector<Item> getEstadoSensores(void) {
     item.valor = (unsigned int)((1.0 - ((sonar_distance - SONAR_MIN_DISTANCE) / (float)(SONAR_MAX_DISTANCE - SONAR_MIN_DISTANCE))) * 100);
   sensors.push_back(item);    
   
-  cout << "\tactivacion-sonar = [ " << item.valor << " / " << (unsigned int)sonar_distance << "cm ]" << endl;
+  cout << "sonar = [ " << item.valor << "% / " << (unsigned int)sonar_distance << "cm ]" << endl;
 
-  cout << "\tactivacion-lines[2] = [ " ;
+  cout << "lines[%] = [ " ;
   for (uint i = 0; i < 2; i++) {
     item.id = sensorsName[7 + i];
     //item.valor = (sensor_data.linefollowing[i] == 255 ? 100 : 0);
@@ -120,7 +118,7 @@ std::vector<Item> getEstadoSensores(void) {
   }
   cout << " ]" << endl;
   
-  cout << "\tactivacion-bumpers[2] = [ " ;
+  cout << "bumpers[%] = [ " ;
   for (uint i = 0; i < 2; i++) {
     item.id = sensorsName[9 + i];
     item.valor = (sensor_data.bumpers[i] == 255 ? 100 : 0);
@@ -161,20 +159,20 @@ void setEstadoActuadores(std::vector<Item> actuators)
 				valor_motores[i] = (valor_motores[i] < 0 ? -1 : 1) * MOTOR_MAX_RANGE;
 
   }
-
 	// elijo bien para mandar o invierto izq-der de motores si es necesario
 	if( actuators[0].id == MOTOR_00 ){ //motor izquierdo
 		//cout << "seteando: " << (int)valor_motores[1] << " " << (int)valor_motores[0] << endl;
-		cout << "\tM = [ " << actuators[0].valor << " % / " << valor_motores[0] << "  ;  " << actuators[1].valor << " % / " << valor_motores[1] << "  ]" << endl;
+		//cout << "\tM = [ " << actuators[0].valor << " % / " << valor_motores[0] << "  ;  " << actuators[1].valor << " % / " << valor_motores[1] << "  ]" << endl;
+		cout << "motores[%] = [ " << actuators[0].valor << " ; " << actuators[1].valor << " ]" << endl;
 		exa_set_motors(valor_motores[1], valor_motores[0]);
 	}
 	else{
 		//cout << "seteando: " << (int)valor_motores[0] << " " << (int)valor_motores[1] << endl;
-		cout << "\tM = [ " << actuators[1].valor << " % / " << valor_motores[1] << "  ;  " << actuators[0].valor << " % / " << valor_motores[0] << "  ]" << endl;
+		//cout << "\tM = [ " << actuators[1].valor << " % / " << valor_motores[1] << "  ;  " << actuators[0].valor << " % / " << valor_motores[0] << "  ]" << endl;
+		cout << "motores[%] = [ " << actuators[1].valor << " ; " << actuators[0].valor << " ]" << endl;
 		exa_set_motors(valor_motores[0], valor_motores[1]);
 	}
- 
-  //cout << "seteando: " << (int)valor_motores[1] << " " << (int)valor_motores[0] << endl;
+   //cout << "seteando: " << (int)valor_motores[1] << " " << (int)valor_motores[0] << endl;
   //exa_set_motors(valor_motores[1], valor_motores[0]);
 }
 
